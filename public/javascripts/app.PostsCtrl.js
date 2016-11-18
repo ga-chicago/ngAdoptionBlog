29f621702a0b131a12bf4efd123eb0e819b663e1
code-for-coffee <jtraverjr@gmail.com>
2016-11-18 10:02:29 -0600

    route changing example inside of a controller

--- public/javascripts/app.PostsCtrl.js
+++ public/javascripts/app.PostsCtrl.js
@@ -2,9 +2,14 @@
  * Created by codeforcoffee on 11/16/16.
  */
 angular.module('adoptionBlog')
-.controller('PostsCtrl', function($scope, $http) {
+.controller('PostsCtrl', function($scope, $http, $location) {
 
   $scope.posts = [];
+
+  $scope.changeRoute = function() {
+    // goto create
+    $location.path('/create');
+  }
 
   $scope.fetch = function() {
     $http.get('http://localhost:9292/api/posts').success(function (results) {
